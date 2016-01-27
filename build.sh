395473a3c700eda7695d6b26a1b602a8e4724034
Jeremy Clement <jeremy.clement@dwarf-labs.com>
2016-01-27 10:54:11 +0100

    update build for buildbot

--- build.sh
+++ build.sh
@@ -1,14 +1,31 @@
-#!/bin/bash
-HERE=`pwd`/build
-if [[ -e $HERE ]];then
-  mkdir HERE
+#!/usr/bin/env bash
+
+PREFIX=$1
+if [ -z "$PREFIX" ]; then
+    PREFIX="`pwd`/build"
+fi
+if [ ! -d $PREFIX ]; then
+  mkdir $PREFIX
 fi
  
 cmake .. -DUSE_PYILMBASE=0 -DBOOST_ROOT=/servers/dwarf_server/DEVELOPMENT/DEVEL_LIBS/libs/boost \
         -DOPENEXR_ROOT=/servers/dwarf_server/DEVELOPMENT/BLENDER_DEPS/openexr-2.1.0 \
         -DILMBASE_ROOT=/servers/dwarf_server/DEVELOPMENT/BLENDER_DEPS/ilmbase \
         -DHDF5_ROOT=/servers/dwarf_server/DEVELOPMENT/DEVEL_LIBS/libs/hdf5 -DUSE_MAYA=1 \
-        -DCMAKE_INSTALL_PREFIX=$HERE -DALEMBIC_SHARED_LIBS=OFF\
+        -DCMAKE_INSTALL_PREFIX=$PREFIX -DALEMBIC_SHARED_LIBS=OFF\
         -DILMBASE_HALF_LIB=Half2Dwarf -DALEMBIC_MAYA_ROOT=/servers/Software/Autodesk/Maya/2014_ext_sp1 \
         -DALEMBIC_MAYA_INC_ROOT=/servers/Software/Autodesk/Maya/2014_ext_sp1/include \
         -DALEMBIC_MAYA_LIB_ROOT=/servers/Software/Autodesk/Maya/2014_ext_sp1/lib
+
+retval=$?
+
+if [ $retval -ne 0 ]; then
+    echo "$retval ERRORS DURING CMAKE"
+    exit $retval
+fi
+
+make && \
+[ -f maya/AbcImport/AbcImport.so ] && \
+make install && \
+[ -f "$PREFIX/maya/plug-ins/AbcImport.so" ] || exit 1
+
